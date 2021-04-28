d420023948790fdd2c764d038239f2655814f25d
WoongWon <woong9711@gmail.com>
2021-04-28 12:22:58 +0900

    210428_GraphicPrograming

--- 게임그래픽프로그래밍2/210428_Light/210428_Light/source.cpp
+++ 게임그래픽프로그래밍2/210428_Light/210428_Light/source.cpp
@@ -49,16 +49,16 @@ struct CUSTOMVERTEX
 // Name: InitD3D()
 // Desc: Initializes Direct3D
 //-----------------------------------------------------------------------------
-HRESULT InitD3D( HWND hWnd )
+HRESULT InitD3D(HWND hWnd)
 {
     // Create the D3D object.
-    if( NULL == ( g_pD3D = Direct3DCreate9( D3D_SDK_VERSION ) ) )
+    if (NULL == (g_pD3D = Direct3DCreate9(D3D_SDK_VERSION)))
         return E_FAIL;
 
     // Set up the structure used to create the D3DDevice. Since we are now
     // using more complex geometry, we will create a device with a zbuffer.
     D3DPRESENT_PARAMETERS d3dpp;
-    ZeroMemory( &d3dpp, sizeof( d3dpp ) );
+    ZeroMemory(&d3dpp, sizeof(d3dpp));
     d3dpp.Windowed = TRUE;
     d3dpp.SwapEffect = D3DSWAPEFFECT_DISCARD;
     d3dpp.BackBufferFormat = D3DFMT_UNKNOWN;
@@ -66,18 +66,18 @@ HRESULT InitD3D( HWND hWnd )
     d3dpp.AutoDepthStencilFormat = D3DFMT_D16;
 
     // Create the D3DDevice
-    if( FAILED( g_pD3D->CreateDevice( D3DADAPTER_DEFAULT, D3DDEVTYPE_HAL, hWnd,
-                                      D3DCREATE_SOFTWARE_VERTEXPROCESSING,
-                                      &d3dpp, &g_pd3dDevice ) ) )
+    if (FAILED(g_pD3D->CreateDevice(D3DADAPTER_DEFAULT, D3DDEVTYPE_HAL, hWnd,
+        D3DCREATE_SOFTWARE_VERTEXPROCESSING,
+        &d3dpp, &g_pd3dDevice)))
     {
         return E_FAIL;
     }
 
     // Turn off culling
-    g_pd3dDevice->SetRenderState( D3DRS_CULLMODE, D3DCULL_NONE );
+    g_pd3dDevice->SetRenderState(D3DRS_CULLMODE, D3DCULL_NONE);
 
     // Turn on the zbuffer
-    g_pd3dDevice->SetRenderState( D3DRS_ZENABLE, TRUE );
+    g_pd3dDevice->SetRenderState(D3DRS_ZENABLE, TRUE);
 
     return S_OK;
 }
@@ -91,13 +91,49 @@ HRESULT InitD3D( HWND hWnd )
 //-----------------------------------------------------------------------------
 HRESULT InitGeometry()
 {
-    // Create the vertex buffer.
-    if( FAILED( g_pd3dDevice->CreateVertexBuffer( 50 * 2 * sizeof( CUSTOMVERTEX ),
-                                                  0, D3DFVF_CUSTOMVERTEX,
-                                                  D3DPOOL_DEFAULT, &g_pVB, NULL ) ) )
+    CUSTOMVERTEX g_Vertices[100];
+    for (DWORD i = 0; i < 50; i++)
+    {
+        FLOAT theta = (2 * D3DX_PI * i) / (50 - 1);
+        g_Vertices[2 * i + 0].position = D3DXVECTOR3(sinf(theta) - 1, -1.0f, cosf(theta));
+        g_Vertices[2 * i + 0].normal = D3DXVECTOR3(sinf(theta) - 1, 0.0f, cosf(theta));
+        g_Vertices[2 * i + 1].position = D3DXVECTOR3(sinf(theta) - 1, 1.0f, cosf(theta));
+        g_Vertices[2 * i + 1].normal = D3DXVECTOR3(sinf(theta) - 1, 0.0f, cosf(theta));
+    }
+
+    if (FAILED(g_pd3dDevice->CreateVertexBuffer(100 * sizeof(CUSTOMVERTEX),
+        0, D3DFVF_CUSTOMVERTEX, D3DPOOL_DEFAULT, &g_pVB, NULL)))
     {
         return E_FAIL;
     }
+
+    VOID* gpVertices;
+    if (FAILED(g_pVB->Lock(0, sizeof(g_Vertices), (void**)&gpVertices, 0)))
+        return E_FAIL;
+    memcpy(gpVertices, g_Vertices, sizeof(g_Vertices));
+    g_pVB->Unlock();
+
+
+    //// Create the vertex buffer.
+    //if( FAILED( g_pd3dDevice->CreateVertexBuffer( 50 * 2 * sizeof( CUSTOMVERTEX ),
+    //                                              0, D3DFVF_CUSTOMVERTEX,
+    //                                              D3DPOOL_DEFAULT, &g_pVB, NULL ) ) )
+    //{
+    //    return E_FAIL;
+    //}
+
+    //CUSTOMVERTEX* pVertices;
+    //if( FAILED( g_pVB->Lock( 0, 0, ( void** )&pVertices, 0 ) ) )
+    //    return E_FAIL;
+    //for( DWORD i = 0; i < 50; i++ )
+    //{
+    //    FLOAT theta = ( 2 * D3DX_PI * i ) / ( 50 - 1 );
+    //    pVertices[2 * i + 0].position = D3DXVECTOR3( sinf( theta )-1, -1.0f, cosf( theta ) );
+    //    pVertices[2 * i + 0].normal =   D3DXVECTOR3( sinf( theta )-1, 0.0f, cosf( theta ) );
+    //    pVertices[2 * i + 1].position = D3DXVECTOR3( sinf( theta )-1, 1.0f, cosf( theta ) );
+    //    pVertices[2 * i + 1].normal =   D3DXVECTOR3( sinf( theta )-1, 0.0f, cosf( theta ) );
+    //}
+    //g_pVB->Unlock();
 
     if (FAILED(g_pd3dDevice->CreateVertexBuffer(50 * 2 * sizeof(CUSTOMVERTEX),
         0, D3DFVF_CUSTOMVERTEX,
@@ -106,31 +142,16 @@ HRESULT InitGeometry()
         return E_FAIL;
     }
 
-    // Fill the vertex buffer. We are algorithmically generating a cylinder
-    // here, including the normals, which are used for lighting.
-    CUSTOMVERTEX* pVertices;
-    if( FAILED( g_pVB->Lock( 0, 0, ( void** )&pVertices, 0 ) ) )
-        return E_FAIL;
-    for( DWORD i = 0; i < 50; i++ )
-    {
-        FLOAT theta = ( 2 * D3DX_PI * i ) / ( 50 - 1 );
-        pVertices[2 * i + 0].position = D3DXVECTOR3( sinf( theta )-1, -1.0f, cosf( theta ) );
-        pVertices[2 * i + 0].normal =   D3DXVECTOR3( sinf( theta )-1, 0.0f, cosf( theta ) );
-        pVertices[2 * i + 1].position = D3DXVECTOR3( sinf( theta )-1, 1.0f, cosf( theta ) );
-        pVertices[2 * i + 1].normal =   D3DXVECTOR3( sinf( theta )-1, 0.0f, cosf( theta ) );
-    }
-    g_pVB->Unlock();
-
     CUSTOMVERTEX* pVertices2;
     if (FAILED(g_pVB2->Lock(0, 0, (void**)&pVertices2, 0)))
         return E_FAIL;
     for (DWORD i = 0; i < 50; i++)
     {
         FLOAT theta = (2 * D3DX_PI * i) / (50 - 1);
-        pVertices2[2 * i + 0].position =    D3DXVECTOR3(sinf(theta)+1, -1.0f, cosf(theta));
-        pVertices2[2 * i + 0].normal =      D3DXVECTOR3(sinf(theta)+1, 0.0f, cosf(theta));
-        pVertices2[2 * i + 1].position =    D3DXVECTOR3(sinf(theta)+1, 1.0f, cosf(theta));
-        pVertices2[2 * i + 1].normal =      D3DXVECTOR3(sinf(theta)+1, 0.0f, cosf(theta));
+        pVertices2[2 * i + 0].position = D3DXVECTOR3(sinf(theta) + 1, -1.0f, cosf(theta));
+        pVertices2[2 * i + 0].normal = D3DXVECTOR3(sinf(theta) + 1, 0.0f, cosf(theta));
+        pVertices2[2 * i + 1].position = D3DXVECTOR3(sinf(theta) + 1, 1.0f, cosf(theta));
+        pVertices2[2 * i + 1].normal = D3DXVECTOR3(sinf(theta) + 1, 0.0f, cosf(theta));
     }
     g_pVB2->Unlock();
 
@@ -146,16 +167,16 @@ HRESULT InitGeometry()
 //-----------------------------------------------------------------------------
 VOID Cleanup()
 {
-    if( g_pVB != NULL )
+    if (g_pVB != NULL)
         g_pVB->Release();
 
     if (g_pVB2 != NULL)
         g_pVB2->Release();
 
-    if( g_pd3dDevice != NULL )
+    if (g_pd3dDevice != NULL)
         g_pd3dDevice->Release();
 
-    if( g_pD3D != NULL )
+    if (g_pD3D != NULL)
         g_pD3D->Release();
 }
 
@@ -169,20 +190,20 @@ VOID SetupMatrices()
 {
     // Set up world matrix
     D3DXMATRIXA16 matWorld;
-    D3DXMatrixIdentity( &matWorld );
-    D3DXMatrixRotationX( &matWorld, timeGetTime() / 500.0f );
-    g_pd3dDevice->SetTransform( D3DTS_WORLD, &matWorld );
+    D3DXMatrixIdentity(&matWorld);
+    D3DXMatrixRotationX(&matWorld, timeGetTime() / 500.0f);
+    g_pd3dDevice->SetTransform(D3DTS_WORLD, &matWorld);
 
     // Set up our view matrix. A view matrix can be defined given an eye point,
     // a point to lookat, and a direction for which way is up. Here, we set the
     // eye five units back along the z-axis and up three units, look at the
     // origin, and define "up" to be in the y-direction.
-    D3DXVECTOR3 vEyePt( 0.0f, 3.0f,-5.0f );
-    D3DXVECTOR3 vLookatPt( 0.0f, 0.0f, 0.0f );
-    D3DXVECTOR3 vUpVec( 0.0f, 1.0f, 0.0f );
+    D3DXVECTOR3 vEyePt(0.0f, 3.0f, -5.0f);
+    D3DXVECTOR3 vLookatPt(0.0f, 0.0f, 0.0f);
+    D3DXVECTOR3 vUpVec(0.0f, 1.0f, 0.0f);
     D3DXMATRIXA16 matView;
-    D3DXMatrixLookAtLH( &matView, &vEyePt, &vLookatPt, &vUpVec );
-    g_pd3dDevice->SetTransform( D3DTS_VIEW, &matView );
+    D3DXMatrixLookAtLH(&matView, &vEyePt, &vLookatPt, &vUpVec);
+    g_pd3dDevice->SetTransform(D3DTS_VIEW, &matView);
 
     // For the projection matrix, we set up a perspective transform (which
     // transforms geometry from 3D view space to 2D viewport space, with
@@ -191,8 +212,8 @@ VOID SetupMatrices()
     // the aspect ratio, and the near and far clipping planes (which define at
     // what distances geometry should be no longer be rendered).
     D3DXMATRIXA16 matProj;
-    D3DXMatrixPerspectiveFovLH( &matProj, D3DX_PI / 4, 1.0f, 1.0f, 100.0f );
-    g_pd3dDevice->SetTransform( D3DTS_PROJECTION, &matProj );
+    D3DXMatrixPerspectiveFovLH(&matProj, D3DX_PI / 4, 1.0f, 1.0f, 100.0f);
+    g_pd3dDevice->SetTransform(D3DTS_PROJECTION, &matProj);
 }
 
 
@@ -209,12 +230,12 @@ VOID SetupLights()
     // Set up a material. The material here just has the diffuse and ambient
     // colors set to yellow. Note that only one material can be used at a time.
     D3DMATERIAL9 mtrl;
-    ZeroMemory( &mtrl, sizeof( D3DMATERIAL9 ) );
+    ZeroMemory(&mtrl, sizeof(D3DMATERIAL9));
     mtrl.Diffuse.r = mtrl.Ambient.r = 1.0f;
     mtrl.Diffuse.g = mtrl.Ambient.g = 1.0f;
     mtrl.Diffuse.b = mtrl.Ambient.b = 1.0f;
     mtrl.Diffuse.a = mtrl.Ambient.a = 1.0f;
-    g_pd3dDevice->SetMaterial( &mtrl );
+    g_pd3dDevice->SetMaterial(&mtrl);
 
     // Set up a white, directional light, with an oscillating direction.
     // Note that many lights may be active at a time (but each one slows down
@@ -234,10 +255,10 @@ VOID SetupLights()
         light.Diffuse.b = 1.0f;
         init = !init;
     }
-    
+
     if (count++ >= 60)
     {
-        switch (rand()%3)
+        switch (rand() % 3)
         {
         case 0:
             light.Diffuse.r += (rand() % 4) + 1;
@@ -251,18 +272,18 @@ VOID SetupLights()
         }
         count = 0;
     }
-    
-    vecDir = D3DXVECTOR3( cosf( timeGetTime() / 350.0f ),
-                          1.0f,
-                          sinf( timeGetTime() / 350.0f ) );
-    D3DXVec3Normalize( ( D3DXVECTOR3* )&light.Direction, &vecDir );
+
+    vecDir = D3DXVECTOR3(cosf(timeGetTime() / 350.0f),
+        1.0f,
+        sinf(timeGetTime() / 350.0f));
+    D3DXVec3Normalize((D3DXVECTOR3*)&light.Direction, &vecDir);
     light.Range = 1000.0f;
-    g_pd3dDevice->SetLight( 0, &light );
-    g_pd3dDevice->LightEnable( 0, TRUE );
-    g_pd3dDevice->SetRenderState( D3DRS_LIGHTING, TRUE );
+    g_pd3dDevice->SetLight(0, &light);
+    g_pd3dDevice->LightEnable(0, TRUE);
+    g_pd3dDevice->SetRenderState(D3DRS_LIGHTING, TRUE);
 
     // Finally, turn on some ambient light.
-    g_pd3dDevice->SetRenderState( D3DRS_AMBIENT, 0x00202020 );
+    g_pd3dDevice->SetRenderState(D3DRS_AMBIENT, 0x00202020);
 }
 
 
@@ -275,11 +296,11 @@ VOID SetupLights()
 VOID Render()
 {
     // Clear the backbuffer and the zbuffer
-    g_pd3dDevice->Clear( 0, NULL, D3DCLEAR_TARGET | D3DCLEAR_ZBUFFER,
-                         D3DCOLOR_XRGB( 0, 0, 255 ), 1.0f, 0 );
+    g_pd3dDevice->Clear(0, NULL, D3DCLEAR_TARGET | D3DCLEAR_ZBUFFER,
+        D3DCOLOR_XRGB(0, 0, 255), 1.0f, 0);
 
     // Begin the scene
-    if( SUCCEEDED( g_pd3dDevice->BeginScene() ) )
+    if (SUCCEEDED(g_pd3dDevice->BeginScene()))
     {
         // Setup the lights and materials
         SetupLights();
@@ -288,9 +309,9 @@ VOID Render()
         SetupMatrices();
 
         // Render the vertex buffer contents
-        g_pd3dDevice->SetStreamSource( 0, g_pVB, 0, sizeof( CUSTOMVERTEX ) );
-        g_pd3dDevice->SetFVF( D3DFVF_CUSTOMVERTEX );
-        g_pd3dDevice->DrawPrimitive( D3DPT_TRIANGLESTRIP, 0, 2 * 50 - 2 );
+        g_pd3dDevice->SetStreamSource(0, g_pVB, 0, sizeof(CUSTOMVERTEX));
+        g_pd3dDevice->SetFVF(D3DFVF_CUSTOMVERTEX);
+        g_pd3dDevice->DrawPrimitive(D3DPT_TRIANGLESTRIP, 0, 2 * 50 - 2);
 
         g_pd3dDevice->SetStreamSource(0, g_pVB2, 0, sizeof(CUSTOMVERTEX));
         g_pd3dDevice->SetFVF(D3DFVF_CUSTOMVERTEX);
@@ -301,7 +322,7 @@ VOID Render()
     }
 
     // Present the backbuffer contents to the display
-    g_pd3dDevice->Present( NULL, NULL, NULL, NULL );
+    g_pd3dDevice->Present(NULL, NULL, NULL, NULL);
 }
 
 
@@ -311,17 +332,17 @@ VOID Render()
 // Name: MsgProc()
 // Desc: The window's message handler
 //-----------------------------------------------------------------------------
-LRESULT WINAPI MsgProc( HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam )
+LRESULT WINAPI MsgProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
-    switch( msg )
+    switch (msg)
     {
-        case WM_DESTROY:
-            Cleanup();
-            PostQuitMessage( 0 );
-            return 0;
+    case WM_DESTROY:
+        Cleanup();
+        PostQuitMessage(0);
+        return 0;
     }
 
-    return DefWindowProc( hWnd, msg, wParam, lParam );
+    return DefWindowProc(hWnd, msg, wParam, lParam);
 }
 
 
@@ -331,43 +352,43 @@ LRESULT WINAPI MsgProc( HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam )
 // Name: WinMain()
 // Desc: The application's entry point
 //-----------------------------------------------------------------------------
-INT WINAPI wWinMain( HINSTANCE hInst, HINSTANCE, LPWSTR, INT )
+INT WINAPI wWinMain(HINSTANCE hInst, HINSTANCE, LPWSTR, INT)
 {
-    UNREFERENCED_PARAMETER( hInst );
+    UNREFERENCED_PARAMETER(hInst);
 
     // Register the window class
     WNDCLASSEX wc =
     {
-        sizeof( WNDCLASSEX ), CS_CLASSDC, MsgProc, 0L, 0L,
-        GetModuleHandle( NULL ), NULL, NULL, NULL, NULL,
+        sizeof(WNDCLASSEX), CS_CLASSDC, MsgProc, 0L, 0L,
+        GetModuleHandle(NULL), NULL, NULL, NULL, NULL,
         L"D3D Tutorial", NULL
     };
-    RegisterClassEx( &wc );
+    RegisterClassEx(&wc);
 
     // Create the application's window
-    HWND hWnd = CreateWindow( L"D3D Tutorial", L"D3D Tutorial 04: Lights",
-                              WS_OVERLAPPEDWINDOW, 100, 100, 300, 300,
-                              NULL, NULL, wc.hInstance, NULL );
+    HWND hWnd = CreateWindow(L"D3D Tutorial", L"D3D Tutorial 04: Lights",
+        WS_OVERLAPPEDWINDOW, 100, 100, 300, 300,
+        NULL, NULL, wc.hInstance, NULL);
 
     // Initialize Direct3D
-    if( SUCCEEDED( InitD3D( hWnd ) ) )
+    if (SUCCEEDED(InitD3D(hWnd)))
     {
         // Create the geometry
-        if( SUCCEEDED( InitGeometry() ) )
+        if (SUCCEEDED(InitGeometry()))
         {
             // Show the window
-            ShowWindow( hWnd, SW_SHOWDEFAULT );
-            UpdateWindow( hWnd );
+            ShowWindow(hWnd, SW_SHOWDEFAULT);
+            UpdateWindow(hWnd);
 
             // Enter the message loop
             MSG msg;
-            ZeroMemory( &msg, sizeof( msg ) );
-            while( msg.message != WM_QUIT )
+            ZeroMemory(&msg, sizeof(msg));
+            while (msg.message != WM_QUIT)
             {
-                if( PeekMessage( &msg, NULL, 0U, 0U, PM_REMOVE ) )
+                if (PeekMessage(&msg, NULL, 0U, 0U, PM_REMOVE))
                 {
-                    TranslateMessage( &msg );
-                    DispatchMessage( &msg );
+                    TranslateMessage(&msg);
+                    DispatchMessage(&msg);
                 }
                 else
                     Render();
@@ -375,7 +396,7 @@ INT WINAPI wWinMain( HINSTANCE hInst, HINSTANCE, LPWSTR, INT )
         }
     }
 
-    UnregisterClass( L"D3D Tutorial", wc.hInstance );
+    UnregisterClass(L"D3D Tutorial", wc.hInstance);
     return 0;
 }
 
